8fc628f6e4e40eed0cf7fb48730c28ede89360c9
IGNI\halfo <half.orange@outlook.com>
2017-12-10 00:38:36 +0200

    Added 'Front' structure with bootstrap and ng-bootstrap. Added main components.

---
+++ Front/angular-src/src/app/app-preferences/app-preferences.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-preferences',
+  templateUrl: './app-preferences.component.html',
+  styleUrls: ['./app-preferences.component.css']
+})
+export class AppPreferencesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}

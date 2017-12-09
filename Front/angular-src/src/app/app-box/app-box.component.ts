b32b40aeba77aa67c32eeb5c01c232a20d199531
IGNI\halfo <half.orange@outlook.com>
2017-12-10 00:38:36 +0200

    Added 'Front' structure with bootstrap and ng-bootstrap. Added main components. + @marius @ionut

---
+++ Front/angular-src/src/app/app-box/app-box.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-box',
+  templateUrl: './app-box.component.html',
+  styleUrls: ['./app-box.component.css']
+})
+export class AppBoxComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}

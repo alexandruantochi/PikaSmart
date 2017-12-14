38d312ca56afa58eaeffcb84e80a3b014dcdc001
IGNI\halfo <half.orange@outlook.com>
2017-12-15 00:28:57 +0200

    Added fluent api contraints for data domain. Removed unused directives.

---
+++ Microservices/Temperature/Persistence/Contraints/ValueConfig.cs
@@ -0,0 +1,16 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Persistence.Contraints
+{
+    class ValueConfig: IEntityTypeConfiguration<TemperatureRecord>
+    {
+        public void Configure(EntityTypeBuilder<TemperatureRecord> builder)
+        {
+            builder.Property(val => val.Value)
+                .IsRequired()
+                .HasColumnName("Value");
+        }
+    }
+}

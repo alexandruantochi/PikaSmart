9dff76f66d26fef2899d3f8ff31ee64b23b411a8
Cazacu Vlad-Gabriel <gabrielcazacu96@gmail.com>
2018-01-11 00:56:51 +0200

    basic local login
    local angular login, can be configured with external RESTful auth (to be made in the dispatcher ?? )

--- Front/angular-src/src/app/app.module.ts
+++ Front/angular-src/src/app/app.module.ts
@@ -1,8 +1,10 @@
 import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
 import {RouterModule} from '@angular/router';
+import {FormsModule} from '@angular/forms';
+import {HttpClientModule, HTTP_INTERCEPTORS} from '@angular/common/http';
 
-
+import {fakeBackendProvider} from './_helpers/index';
 import {AppRoutingModule} from './app.routing';
 import {ComponentsModule} from './components/components.module';
 import {AppComponent} from './app.component';
@@ -10,20 +12,48 @@ import {DashboardComponent} from './dashboard/dashboard.component';
 import {UserProfileComponent} from './user-profile/user-profile.component';
 import {NotificationsComponent} from './notifications/notifications.component';
 
+import {AlertComponent} from './_directives/index';
+import {AuthGuard} from './_guards/index';
+import {JwtInterceptor} from './_helpers/index';
+import {AlertService, AuthenticationService, UserService} from './_services/index';
+import {HomeComponent} from './home/index';
+import {LoginComponent} from './login/index';
+import {RegisterComponent} from './register/index';
+
+
 @NgModule({
   declarations: [
     AppComponent,
     DashboardComponent,
     UserProfileComponent,
-    NotificationsComponent
+    NotificationsComponent,
+    AlertComponent,
+    HomeComponent,
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
+    FormsModule,
+    HttpClientModule,
     ComponentsModule,
     RouterModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [
+    AuthGuard,
+    AlertService,
+    AuthenticationService,
+    UserService,
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: JwtInterceptor,
+      multi: true
+    },
+
+    // provider used to create fake backend
+    fakeBackendProvider
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule {
--- Front/angular-src/src/app/app.routing.ts
+++ Front/angular-src/src/app/app.routing.ts
@@ -1,15 +1,25 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule  } from '@angular/platform-browser';
-import { Routes, RouterModule } from '@angular/router';
-import { DashboardComponent } from './dashboard/dashboard.component';
-import { UserProfileComponent } from './user-profile/user-profile.component';
-import { NotificationsComponent } from './notifications/notifications.component';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {Routes, RouterModule} from '@angular/router';
+import {DashboardComponent} from './dashboard/dashboard.component';
+import {UserProfileComponent} from './user-profile/user-profile.component';
+import {NotificationsComponent} from './notifications/notifications.component';
 
-const routes: Routes =[
-    { path: 'dashboard',      component: DashboardComponent },
-    { path: 'user-profile',   component: UserProfileComponent },
-    { path: 'notifications',  component: NotificationsComponent },
-    { path: '',               redirectTo: 'dashboard', pathMatch: 'full' }
+import {HomeComponent} from './home/index';
+import {LoginComponent} from './login/index';
+import {RegisterComponent} from './register/index';
+import {AuthGuard} from './_guards/index';
+
+const routes: Routes = [
+  {path: 'dashboard', component: DashboardComponent, canActivate: [AuthGuard]},
+  {path: 'user-profile', component: UserProfileComponent, canActivate: [AuthGuard]},
+  {path: 'notifications', component: NotificationsComponent},
+  {path: '', component: HomeComponent, canActivate: [AuthGuard]},
+  {path: 'login', component: LoginComponent},
+  {path: 'register', component: RegisterComponent},
+
+  // otherwise redirect to home
+  {path: '**', redirectTo: ''}
 ];
 
 @NgModule({
@@ -17,7 +27,7 @@ const routes: Routes =[
     BrowserModule,
     RouterModule.forRoot(routes)
   ],
-  exports: [
-  ],
+  exports: [],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
